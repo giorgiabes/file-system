f1306950a4aa5715d2ad5de1c27490a513ecf860
giorgiabes <abesadze.george@gmail.com>
2026-01-13 05:25:26 +0400

    add FileNode.ts

---
+++ packages/core/src/domain/entities/FileNodes.ts
@@ -0,0 +1,55 @@
+/**
+ * File: FileNode.ts
+ *
+ * Purpose: Represents a file with metadata
+ */
+import FilePath from "../value-objects/FilePath";
+import ContentHash from "../value-objects/ContentHash";
+
+class FileNode {
+  private readonly path: FilePath; // (identity - never changes)
+  private contentHash: ContentHash; // (can change if file updated)
+  private size: number;
+  private createdAt: Date;
+  private modifiedAt: Date;
+
+  constructor(path: FilePath, contentHash: ContentHash, size: number) {
+    this.path = path;
+    this.contentHash = contentHash;
+    this.size = size;
+    this.createdAt = new Date();
+    this.modifiedAt = new Date();
+  }
+
+  getPath(): FilePath {
+    return this.path;
+  }
+
+  getContentHash(): ContentHash {
+    return this.contentHash;
+  }
+
+  getSize(): number {
+    return this.size;
+  }
+
+  getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  getModifiedAt(): Date {
+    return this.modifiedAt;
+  }
+
+  updateContent(newHash: ContentHash, newSize: number): void {
+    this.contentHash = newHash;
+    this.size = newSize;
+    this.modifiedAt = new Date();
+  }
+
+  touch(): void {
+    this.modifiedAt = new Date();
+  }
+}
+
+export default FileNode;
